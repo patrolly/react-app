f6d5954273c1735c0cf3b9b7615a05d3b9548b1b
patrolly <pushkinek@interia.pl>
2025-11-21 18:03:11 +0100

    first commit

---
+++ src/components/likeCounter.js
@@ -0,0 +1,10 @@
+import './likeCounter.css';
+function LikeCounter(){
+    return(
+        <div className='post'>
+            <p>Fajnyyy post!! Liczba likeów ?</p>
+            <button>Polub ❤️</button>
+        </div>
+    );
+}
+export default LikeCounter;
